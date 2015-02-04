da8bb50a22843bcf328957f0ff50c0933fecaf87
cjencks <claire.jencks@capitalone.com>
2015-02-04 14:28:10 -0800

    Initial Commit

---
+++ Algorythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorythm
+//
+//  Created by Jencks, Claire on 2/4/15.
+//  Copyright (c) 2015 Jencks, Claire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
